a3a1f68f85ce7baa8a282bf4d816301307b54bf2
matp5 <mpietak@vp.pl>
2018-12-13 19:25:37 +0100

    first commit

---
+++ js/menuButton.js
@@ -0,0 +1,23 @@
+let headerBg = document.getElementById('header');
+let menuBtn = document.getElementById('menu-btn');
+
+window.addEventListener('resize', resize);
+headerBg.style.backgroundColor = 'white';
+
+function resize() {
+    if (window.innerWidth > 992) {
+        menuBtn.removeEventListener('click', headerColor);
+        headerBg.style.backgroundColor = 'white';
+    } else {
+        headerBg.style.backgroundColor = 'transparent';
+    }
+};
+
+function headerColor() {
+    if (headerBg.style.backgroundColor == 'white') {
+        headerBg.style.backgroundColor = 'transparent';
+    } else {
+        headerBg.style.backgroundColor = 'white';
+    }
+
+};
---
+++ js/menuSwitch.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.nav-link').on('click', function () {
+        $('.active-content').removeClass('active-content').addClass('inactive-content');
+
+        var active = $(this).attr('href');
+        $(active).removeClass('inactive-content').addClass('active-content');
+    });
+});
